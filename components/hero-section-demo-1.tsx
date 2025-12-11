c0df90aac9e5d71e1fa9740d5d07a27a5dfd21f2
NattapatJanchaysang <66011360@kmitl.ac.th>
2025-12-11 12:28:43 +0700

    Wait for the poster

--- components/hero-section-demo-1.tsx
+++ components/hero-section-demo-1.tsx
@@ -6,6 +6,7 @@ import { FeatureBentoGrid } from "@/app/_components/FeatureBentoGrid";
 import { UserButton, useUser } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function HeroSectionOne() {
   return (
@@ -90,11 +91,8 @@ export default function HeroSectionOne() {
 const Navbar = () => {
   const {user} = useUser()
   return (
-    <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
-      <div className="flex items-center gap-2">
-        <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
-        <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
-      </div>
+    <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 py-4 dark:border-neutral-800 px-10 md:px-24 lg:px-40">
+      <Link href='/' className="cursor-pointer"><Image src="/Websitelogo.png" alt="logo" width={180} height={10}/></Link>
       {!user? <Link href={'/sign-in'}><button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Login
       </button></Link> : 
